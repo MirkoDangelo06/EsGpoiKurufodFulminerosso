57db45064d131aa06a72527b9600d14c67962603
MirkoDangelo06 <dangelo.mirko@itismeucci.com>
2025-02-07 10:36:15 +0100

    initialize getter and setter and constructor

--- src/Time.java
+++ src/Time.java
@@ -1,3 +1,45 @@
 public class Time {
+    protected int ore;
+    protected int minuti;
+    protected int secondi;
+
     
+
+
+
+    public Time(int ore, int minuti, int secondi) {
+        if(ore >24 || minuti >60 || secondi > 60){
+            this.ore = 0;
+            this.minuti=0;
+            this.secondi=0;
+        }else{
+            this.ore = ore;
+            this.minuti = minuti;
+            this.secondi = secondi;
+        }
+    }
+
+    public int getOre() {
+        return ore;
+    }
+    public void setOre(int ore) {
+        this.ore = ore;
+    }
+    public int getMinuti() {
+        return minuti;
+    }
+    public void setMinuti(int minuti) {
+        this.minuti = minuti;
+    }
+    public int getSecondi() {
+        return secondi;
+    }
+    public void setSecondi(int secondi) {
+        this.secondi = secondi;
+    }
+
+
+    
+
+
 }
